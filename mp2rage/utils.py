758cdc06e522b10e6cbb466577545f3977320e70
saundersresearch <adam.m.saunders@vanderbilt.edu>
2023-08-08 14:08:04 -0500

    Created plots for MP2RAGE parameters

--- mp2rage/utils.py
+++ mp2rage/utils.py
@@ -1,13 +1,11 @@
 import numpy as np
 
-def gre(M0=7.0, T1=1, TA=1, TB=1, TC=1, TR=6e-3, alpha_1=4, alpha_2=4, n=36, MP2RAGE_TR=6, eff=0.96):
+def GRE(T1=1, TA=1, TB=1, TC=1, TR=6e-3, alpha_1=4, alpha_2=4, n=36, MP2RAGE_TR=6, eff=0.96):
     """
     Returns the values for the gradient echo blocks GRE1 and GRE2.
 
     Parameters
     ---------
-    M0 : arraylike, optional, default=7.0
-        The net magnetization in T
     T1 : arraylike, optional, default=1.0
         T1 relaxation time in s
     TA : arraylike, optional, default=1.0
@@ -36,12 +34,12 @@ def gre(M0=7.0, T1=1, TA=1, TB=1, TC=1, TR=6e-3, alpha_1=4, alpha_2=4, n=36, MP2
     GRE2: ndarray
         Second gradient echo block
     """
-    params = [M0, T1, TA, TB, TC, TR, alpha_1, alpha_2, n, MP2RAGE_TR, eff]
+    params = [T1, TA, TB, TC, TR, alpha_1, alpha_2, n, MP2RAGE_TR, eff]
 
     # Convert to Numpy arrays
     for i, p in enumerate(params):
         params[i] = np.asarray([p]) if np.isscalar(p) else np.asarray(p)
-    (M0, T1, TA, TB, TC, TR, alpha_1, alpha_2, n, MP2RAGE_TR, eff) = tuple(params)
+    (T1, TA, TB, TC, TR, alpha_1, alpha_2, n, MP2RAGE_TR, eff) = tuple(params)
 
     # Calculate exponential terms
     E1 = np.exp(-TR/T1)
@@ -50,11 +48,11 @@ def gre(M0=7.0, T1=1, TA=1, TB=1, TC=1, TR=6e-3, alpha_1=4, alpha_2=4, n=36, MP2
     EC = np.exp(-TC/T1)
     
     # Calculate steady-state magnetization
-    mz_ss = M0*(((((1-EA)*np.power(np.cos(alpha_1)*E1, n) + (1-E1)*(1-np.power(np.cos(alpha_1)*E1, n))/(1-np.cos(alpha_1)*E1))*EB + (1-EB))*np.power(np.cos(alpha_2)*E1, n) + (1-E1)*(1-np.power(np.cos(alpha_2)*E1, n))/(1-np.cos(alpha_2)*E1))*EC + (1-EC))/(1 + eff*np.power(np.cos(alpha_1)*np.cos(alpha_2), n)*np.exp(-MP2RAGE_TR/T1))
+    mz_ss = (((((1-EA)*np.float_power(np.cos(alpha_1)*E1, n) + (1-E1)*(1-np.float_power(np.cos(alpha_1)*E1, n))/(1-np.cos(alpha_1)*E1))*EB + (1-EB))*np.float_power(np.cos(alpha_2)*E1, n) + (1-E1)*(1-np.float_power(np.cos(alpha_2)*E1, n))/(1-np.cos(alpha_2)*E1))*EC + (1-EC))/(1 + eff*np.float_power(np.cos(alpha_1)*np.cos(alpha_2), n)*np.exp(-MP2RAGE_TR/T1))
     
     # Calculate gradient echo blocks
-    GRE1 = np.sin(alpha_1)*(((-eff*mz_ss)/(M0)*EA + (1-EA))*np.power(np.cos(alpha_1)*E1, n/2-1) + (1-E1)*(1-np.power(np.cos(alpha_1)*E1, n/2-1))/(1-np.cos(alpha_1)*E1))
-    GRE2 = np.sin(alpha_2)*((mz_ss/M0 - (1-EC))/(EC*np.power(np.cos(alpha_2)*E1, n/2)) - (1-E1)*((np.power(np.cos(alpha_2)*E1, -n/2)-1)/(1 - np.cos(alpha_2)*E1)))
+    GRE1 = np.sin(alpha_1)*((-eff*mz_ss*EA + (1-EA))*np.float_power(np.cos(alpha_1)*E1, n/2-1) + (1-E1)*(1-np.float_power(np.cos(alpha_1)*E1, n/2-1))/(1-np.cos(alpha_1)*E1))
+    GRE2 = np.sin(alpha_2)*((mz_ss - (1-EC))/(EC*np.float_power(np.cos(alpha_2)*E1, n/2)) - (1-E1)*((np.float_power(np.cos(alpha_2)*E1, -n/2)-1)/(1 - np.cos(alpha_2)*E1)))
     
     return GRE1, GRE2
 
