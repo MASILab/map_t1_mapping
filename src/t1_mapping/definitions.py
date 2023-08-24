7c9609209812c214e69c83a0cb5da6b0b524cd38
saundersresearch <adam.m.saunders@vanderbilt.edu>
2023-08-24 13:52:49 -0500

    Reorganized examples and notebooks

---
+++ src/t1_mapping/definitions.py
@@ -0,0 +1,3 @@
+import os
+
+DATA = '/nfs/masi/saundam1/outputs/mp2rage_converted_v2023'
--- src/t1_mapping/mp2rage.py
+++ src/t1_mapping/mp2rage.py
@@ -1,23 +1,42 @@
 import t1_mapping.utils
+from functools import cached_property
+import nibabel as nib
+import numpy as np
 
 class MP2RAGEFitter():
-    def __init__(self):
+    def __init__(self, inv1, inv2, acq_params):
         """
         Fitter for calculations using an MP2RAGE sequence
 
         Parameters
         ----------
-        inv1 : str
-            Filename of first gradient echo readout
-        inv2 : str
-            Filename of second gradient echo readout
+        inv1 : nibabel.nifti1.Nifti1Image or nibabel.nifti2.Nifti2Image
+            First gradient echo readout, stored as a NIFTI image
+        inv2 : nibabel.nifti1.Nifti1Image or nibabel.nifti2.Nifti2Image
+            Second gradient echo readout, stored as a NIFTI image
         acq_params : Dict
             Dictionary containing acquisition parameters
 
         Attributes
         ----------
-        t1_w : nibabel.nifti2.Nifti2Image
+        t1w : nibabel.nifti2.Nifti2Image
             T1-weighted MP2RAGE image
         t1_map : nibabel.nifti2.Nifti2Image
             Quantitative T1 map
         """
+        self.inv1 = inv1
+        self.inv2 = inv2
+        self.acq_params = acq_params
+
+    @cached_property
+    def _inv1_data(self):
+        return np.asanyarray(self.inv1.dataobj)
+    
+    @cached_property
+    def _inv2_data(self):
+        return np.asanyarray(self.inv2.dataobj)
+
+    @cached_property
+    def t1w(self):
+        t1w_array = t1_mapping.utils.mp2rage_t1w(self._inv1_data, self._inv2_data)
+        return nib.nifti2.Nifti2Image(t1w_array, self.inv1.affine)
