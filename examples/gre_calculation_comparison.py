f8629f28c03c0ccc3c6ad418aac78ff5ff3e2941
saundersresearch <adam.m.saunders@vanderbilt.edu>
2023-09-11 13:08:47 -0500

    Constrained timing parameters in gre_signal

--- examples/gre_calculation_comparison.py
+++ examples/gre_calculation_comparison.py
@@ -5,15 +5,14 @@ from t1_mapping.utils import gre_signal
 T1 = np.linspace(0.5, 5, 1000)
 
 input_params = {
-    "TA": 1,
-    "TB": 1,
-    "TC": 1,
+    "TA": 0.335,
+    "TB": 0.95,
     "TR": 6e-3,
     "alpha_1": 4,
     "alpha_2": 4,
-    "n": 36,
-    "MP2RAGE_TR": 6,
-    "eff": 0.96
+    "n": 225,
+    "MP2RAGE_TR": 8.25,
+    "eff": 0.84
 }
 
 GRE1_orig, GRE2_orig = gre_signal(T1=T1, **input_params, method='code')
