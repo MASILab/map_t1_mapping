9e3d06e3d5f81b2fcc22150ec4b104b2f61d4617
saundersresearch <adam.m.saunders@vanderbilt.edu>
2023-10-06 16:43:22 -0500

    Work on loading ground truth

--- src/t1_mapping/definitions.py
+++ src/t1_mapping/definitions.py
@@ -2,3 +2,4 @@ import os
 
 DATA = '/nfs/masi/saundam1/outputs/mp2rage_converted_v2023'
 SIMULATION_DATA = '/nfs/masi/saundam1/outputs/t1_mapping/distr'
+GROUND_TRUTH = '/nfs/masi/saundam1/datasets/MP2RAGE_SIR_qMT/7T_SIRqMT_R1maps'
