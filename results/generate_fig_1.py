fixed posterior distribution calculation

results/generate_fig_1.py
@@ -30,7 +30,7 @@ GRE = t1_mapping.utils.gre_signal(
 m = t1_mapping.utils.mp2rage_t1w(GRE[0,:], GRE[1,:])
 
 # Plot
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 6))
 ax1 = fig.add_subplot(1,2,1)
 ax1.plot(m, subj.t1)
 ax1.set_xlabel('$S_{1,2}$')
@@ -54,7 +54,9 @@ ax2.plot(m2, subj.t1, zs=0.5, zdir='y', color=[1,0,0,0.2])
 t1w1 = subj.mp2rage[0].get_fdata()
 t1w2 = subj.mp2rage[1].get_fdata()
 indx = np.arange(0, len(t1w1.flatten()), 50000)
-ax2.scatter(t1w1.flatten()[indx], t1w2.flatten()[indx], zdir='z', color=[0,1,0,0.2], label='Unknown values')
+ax2.scatter(t1w1.flatten()[indx], t1w2.flatten()[indx], zdir='z', color=[0,1,0,0.2], label='Sample unknown values')
 ax2.legend()
 ax2.set_title('Multiple MP2RAGE Signals')
+
+fig.savefig('/home/saundam1/VM/shared_folder/mp2rage/ISMRM_figures/Figure_1.tiff', dpi=600)
 plt.show()

src/t1_mapping/utils.py
@@ -401,12 +401,16 @@ def mp2rage_t1_exp_val(t1, delta_t1, m, m_ranges, delta_m, inv, TD, TR, flip_ang
     n_pairs = len(pairs)
     n_readouts = len(inv)
 
+    # Calculate likelihood
+    likelihood = counts / np.sum(counts * np.prod(delta_m), axis=tuple(range(n_pairs)))
+    likelihood = np.nan_to_num(likelihood, nan=0)
+
     # Calculate posterior
-    posterior = counts / np.sum(counts * np.prod(delta_m), axis=tuple(range(n_pairs)))
+    posterior = likelihood / np.sum(delta_t1*likelihood, axis=-1)[:,:,np.newaxis]
     posterior = np.nan_to_num(posterior, nan=0)
 
-    # Expected value (Need to reverse T1)
-    exp_val = np.sum(t1*posterior,axis=-1)/np.sum(posterior,axis=-1)
+    # Expected value
+    exp_val = np.sum(t1*posterior*delta_t1,axis=-1)
     exp_val = np.nan_to_num(exp_val, nan=0)
 
     # Create grid
@@ -470,22 +474,23 @@ def mp2rage_t1_var(t1, delta_t1, m, m_ranges, delta_m, inv, TD, TR, flip_angles,
     n_pairs = len(pairs)
     n_readouts = len(inv)
 
+    # Calculate likelihood
+    likelihood = counts / np.sum(counts * np.prod(delta_m), axis=tuple(range(n_pairs)))
+    likelihood = np.nan_to_num(likelihood, nan=0)
+
     # Calculate posterior
-    posterior = counts / np.sum(counts * np.prod(delta_m), axis=tuple(range(n_pairs)))
+    posterior = likelihood / np.sum(delta_t1*likelihood, axis=-1)[:,:,np.newaxis]
     posterior = np.nan_to_num(posterior, nan=0)
 
-    # Expected value (Need to reverse T1)
-    # exp_val = np.sum(t1[::-1,np.newaxis,np.newaxis]*posterior, axis=-1)/np.sum(posterior, axis=2)
-    exp_val = np.sum(t1*posterior,axis=-1)/np.sum(posterior,axis=-1)
+    # Expected value
+    exp_val = np.sum(t1*posterior*delta_t1,axis=-1)
     exp_val = np.nan_to_num(exp_val, nan=0)
 
-    # Variance (Need to reverse T1)
-    var = np.sum((t1 - exp_val)**2*posterior, axis=-1)/np.sum(posterior, axis=-1)
-    # var = np.sum(t1**2*posterior,axis=-1)/np.sum(posterior,axis=-1) - exp_val**2
-    var = np.nan_to_num(var, nan=0)
+    # Variance
+    variance = np.sum((t1 - exp_val[:,:,np.newaxis])**2*posterior*delta_t1, axis=-1)
 
     # Create grid
-    interp = RegularGridInterpolator(tuple(m), values=var,
+    interp = RegularGridInterpolator(tuple(m), values=variance,
         bounds_error=False, fill_value=0, method='linear')
 
     # Calculate MP2RAGE images to get values at