976b6b8f83f31fa6ac0fbedf492789e827c34aab
saundersresearch <adam.m.saunders@vanderbilt.edu>
2023-12-06 16:43:29 -0600

    SLANT-TICV sanity checks with BLSA

--- segment_t1w_slant.sh
+++ segment_t1w_slant.sh
@@ -1,23 +1,24 @@
 # Segment T1w images using SLANT
 
 # Loop over subjects
-for input_folder in /home/saundam1/temp_data/mp2rage_t1w_norm_ss/334264; do
+for input_folder in /home/saundam1/temp_data/mp2rage_t1w_slant_ss/*; do
+    echo $input_folder
     subj_id=`basename $input_folder`
-    output_folder="/home/saundam1/temp_data/mp2rage_t1w_norm_slant_ss/$subj_id"
+#    output_folder="/home/saundam1/temp_data/mp2rage_t1w_norm_slant_ss/$subj_id"
     
-    mkdir -p $output_folder $output_folder/dl $output_folder/pre $output_folder/post
+    #mkdir -p $output_folder $output_folder/dl $output_folder/pre $output_folder/post
     
-    input_folder=/home/saundam1/temp_data/slant_test3/in
-    output_folder=/home/saundam1/temp_data/slant_test3/out
+    #input_folder=/home/saundam1/temp_data/mp2rage_t1w_slant_ss/334264/in
+    #output_folder=/home/saundam1/temp_data/mp2rage_t1w_slant_ss/334264/out
     # fslmaths /nfs/masi/saundam1/outputs/t1_mapping/mp2rage_t1w/$subj_id/t1w.nii.gz -mas /nfs/masi/saundam1/outputs/t1_mapping/t1w_strip/$subj_id/mask.nii $output_folder/t1w.nii.gz
 
     # Run singularity
     #singularity exec --nv -B $input_folder:/INPUTS/ -B $output_folder:/OUTPUTS/ /home/saundam1/singularity/slant_gpu_v1.1.0.simg /extra/run_deep_brain_seg.sh
-    echo $input_folder
+    #echo $input_folder
 
     #export cmd="/home/local/VANDERBILT/caily/Apps/singularity/singularity-ce-3.8.0/bin/singularity exec -B $input_folder:/opt/slant/matlab/input_pre -B $input_folder:/opt/slant/matlab/input_post -B $output_folder/pre:/opt/slant/matlab/output_pre -B $output_folder/dl:/opt/slant/dl/working_dir -B $output_folder/post:/opt/slant/matlab/output_post -e /home/saundam1/singularity/ssSLANT_v4.0.sif /opt/slant/run.sh"
 
-    export cmd="/home/local/VANDERBILT/caily/Apps/singularity/singularity-ce-3.8.0/bin/singularity exec -B $input_folder/pre:/opt/slant/matlab/input_pre -B $input_folder/post:/opt/slant/matlab/input_post -B $output_folder/pre:/opt/slant/matlab/output_pre -B $output_folder/dl:/opt/slant/dl/working_dir -B $output_folder/post:/opt/slant/matlab/output_post -e /home/saundam1/singularity/ssSLANT_v4.0.sif /opt/slant/run.sh"
+    export cmd="/home/local/VANDERBILT/caily/Apps/singularity/singularity-ce-3.8.0/bin/singularity exec -B $input_folder/in/pre:/opt/slant/matlab/input_pre -B $input_folder/in/post:/opt/slant/matlab/input_post -B $input_folder/out/pre:/opt/slant/matlab/output_pre -B $input_folder/out/dl:/opt/slant/dl/working_dir -B $input_folder/out/post:/opt/slant/matlab/output_post -e /home/saundam1/singularity/ssSLANT_v4.0.sif /opt/slant/run.sh"
     echo $cmd
     $cmd
 
