4c1dd1b64619351f99bdfe3c8e3baef6999e8fee
saundersresearch <adam.m.saunders@vanderbilt.edu>
2023-12-11 17:07:32 -0600

    creating robust T1w

---
+++ create_robust_t1w.py
@@ -0,0 +1,118 @@
+import nibabel as nib
+from adam_utils.nifti import plot_nifti
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+# # Load GREs
+# scan_folder = '/nfs/masi/saundam1/outputs/t1_mapping/mp2rage_converted_v2023/334264/401-x-WIPMP2RAGE_0p7mm_1sTI_best_oneSENSE-x-WIPMP2RAGE_0p7mm_1sTI_best_oneSENSE'
+# gre1_real = nib.load(os.path.join(scan_folder, '401_real_t1010.nii'))
+# gre1_imag = nib.load(os.path.join(scan_folder, '401_imaginary_t1010.nii'))
+# gre2_real = nib.load(os.path.join(scan_folder, '401_real_t3310.nii'))
+# gre2_imag = nib.load(os.path.join(scan_folder, '401_imaginary_t3310.nii'))
+
+# gre1 = gre1_real.get_fdata() + 1j*gre1_imag.get_fdata()
+# gre2 = gre2_real.get_fdata() + 1j*gre2_imag.get_fdata()
+
+# # Create robust T1-weighted image
+# t1w = np.real(np.conj(gre1)*gre2)/(np.abs(gre1)**2 + np.abs(gre2)**2)
+# mask = nib.load('/nfs/masi/saundam1/outputs/t1_mapping/t1w_strip/334264/mask.nii')
+# denom_masked = (np.abs(gre1)**2 + np.abs(gre2)**2) * mask.get_fdata()
+# beta = np.mean(denom_masked)
+# robust_t1w = (np.real(np.conj(gre1)*gre2) - beta)/(np.abs(gre1)**2 + np.abs(gre2)**2 + 2*beta)
+# robust_t1w += 0.5
+
+# t1w_nifti = nib.Nifti1Image(t1w, affine=gre1_real.affine)
+# robust_t1w_nifti = nib.Nifti1Image(robust_t1w, affine=gre1_real.affine)
+
+# # Save to NIFTI
+# robust_t1w_nifti.to_filename('/nfs/masi/saundam1/outputs/t1_mapping/test/robust_t1w.nii.gz')
+
+# Create MP2RAGE data
+import os
+import t1_mapping
+import nibabel as nib
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import re
+
+# Load groups
+groups = pd.read_excel(os.path.join(t1_mapping.definitions.GROUND_TRUTH_MAT, 'scanID_groups.xlsx'))
+control_subj = groups['Health Control Scans'].dropna().astype(np.int64)
+ms_subj = groups['MS Patient Scans'].dropna().astype(np.int64)
+ground_truth = pd.read_csv(os.path.join(t1_mapping.definitions.GROUND_TRUTH, 'ground_truth_subjects.csv'))
+
+# Loop through subjects
+for subject in tqdm(os.listdir(t1_mapping.definitions.DATA)):
+    subj_id = int(subject)
+    subj_path = os.path.join(t1_mapping.definitions.OUTPUTS, 'robust_t1w', subject)
+    data_dir = os.path.join(t1_mapping.definitions.OUTPUTS, 'mp2rage_converted_v2023', subject)
+    if subj_id in ms_subj.to_numpy():
+        group = 'ms'
+    elif subj_id in control_subj.to_numpy():
+        group = 'control'
+    else:
+        print(f'Skipping {subj_id}')
+        group = 'n/a'
+        continue
+
+    if os.path.exists(subj_path) and 't1w.nii.gz' in os.listdir(subj_path):
+        print(f'Already did {subj_id}')
+        continue
+
+    if subj_id not in ground_truth['Subject'].values:
+        print(f'{subj_id} not in ground truth')
+        continue
+
+    # Get list of scans
+    scan = os.listdir(os.path.join(t1_mapping.definitions.DATA, subject))
+
+    # Get scan IDs
+    scan_id = [s.split('-')[0] for s in scan]
+    primary_scan_ids = [int(s) for s in scan_id if s.endswith('1')]
+    primary_scan_ids = sorted(primary_scan_ids)
+    highest_primary_scan_id = primary_scan_ids[-1]
+
+    # Get scan that starts with highest primary scan ID
+    chosen_scan = [s for s in scan if s.startswith(str(highest_primary_scan_id))][0]
+    print(f'{chosen_scan=}')
+
+    # Find scan times
+    data_files = os.listdir(os.path.join(data_dir, chosen_scan))
+    print(f'{data_files=}')
+
+    # Get scan times from files
+    times = [re.findall(r'\d{4}(?=\.)', s)[0] for s in data_files]
+
+    # Get unique items and sort
+    times = list(set(times))
+    times = sorted(t for t in times)
+    times = [times[0], times[1]]
+    print(f'{subj_id}: {times}')
+
+    # Create MP2RAGE subject
+    subj = t1_mapping.mp2rage.MP2RAGESubject(
+        subject_id=subject,
+        scan=chosen_scan,
+        scan_times=times
+    )
+
+    # Calculate T1 map and save
+    os.makedirs(subj_path, exist_ok=True)
+
+    # Make robust T1-weighted image
+    gre1 = subj.inv[0].get_fdata(dtype=np.complex64)
+    gre2 = subj.inv[1].get_fdata(dtype=np.complex64)
+    t1w = np.real(np.conj(gre1)*gre2)/(np.abs(gre1)**2 + np.abs(gre2)**2)
+
+    # Get beta value by mean value of denominator in brain
+    mask = nib.load(os.path.join(t1_mapping.definitions.OUTPUTS, 't1w_strip', subject, 'mask.nii'))
+    denom_masked = (np.abs(gre1)**2 + np.abs(gre2)**2) * mask.get_fdata()
+    beta = np.mean(denom_masked)
+    robust_t1w = (np.real(np.conj(gre1)*gre2) - beta)/(np.abs(gre1)**2 + np.abs(gre2)**2 + 2*beta)
+    robust_t1w += 0.5
+    robust_t1w_nifti = nib.Nifti1Image(robust_t1w, affine=subj.affine)
+
+    # Save to NIFTI
+    robust_t1w_nifti.to_filename(os.path.join(subj_path, 'robust_t1w.nii.gz'))
--- slant/move_slant_nss_files.py
+++ slant/move_slant_nss_files.py
@@ -1,47 +1,25 @@
 import os
 import csv
+import t1_mapping
 
-def find_mp2rage_files(root_dir, csv_path, column_name):
-    mp2rage_files = []
-    
-    # Read the CSV file and get the subject IDs from the specified column
-    subject_ids = set()
-    with open(csv_path, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            subject_ids.add(row[column_name])
+csv_path = '/nfs/masi/saundam1/datasets/MP2RAGE_SIR_qMT/ground_truth_subjects.csv'
 
-    # Walk through the root directory and its subdirectories
-    for dirpath, dirnames, filenames in os.walk(root_dir):
-        # Get the subject ID (the first folder after root_directory)
-        subject_folder = os.path.relpath(dirpath, root_dir).split(os.path.sep)[0]
+subject_ids = set()
+with open(csv_path, 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        subject_ids.add(row['Subject'])
 
-        # Check if the subject ID is in the CSV's column
-        if subject_folder in subject_ids:
-            # Check if the folder name contains the specified string
-            if 'mp2rage_v2_1' in dirpath:
-                # Check if the required file is present in the directory
-                if 'mp2rage.nii.gz' in filenames:
-                    # Construct the full file path
-                    file_path = os.path.join(dirpath, 'mp2rage.nii.gz')
+# Copy over robust T1 
+slant_path = '/home/saundam1/temp_data/slant_mp2rage_nss'
+t1w_path = '/nfs/masi/saundam1/outputs/t1_mapping/robust_t1w'
+for subj_id in subject_ids:
+    print(subj_id)
+    file_path = os.path.join(t1w_path, str(subj_id), 'robust_t1w.nii.gz')
+    os.makedirs(os.path.join(slant_path, str(subj_id), 'in'), exist_ok=True)
+    os.makedirs(os.path.join(slant_path, str(subj_id), 'out', 'pre'), exist_ok=True)
+    os.makedirs(os.path.join(slant_path, str(subj_id), 'out', 'post'), exist_ok=True)
+    os.makedirs(os.path.join(slant_path, str(subj_id), 'out', 'dl'), exist_ok=True)
 
-                    # Exceptions
-                    if subject_folder == '335586' and file_path == '/nfs/masi/saundam1/datasets/MP2RAGE/335586/335586/MP2RAGE-x-335586-x-335586-x-mp2rage_v2_1-x-8ab013bf-ba2e-4d76-bf28-8c018abe13c0/MP2RAGE/mp2rage.nii.gz':
-                        continue
-                    
-                    # Make directories
-                    print(file_path)
-                    slant_path = '/home/saundam1/temp_data/slant_mp2rage_nss'
-                    os.makedirs(os.path.join(slant_path, subject_folder, 'in'), exist_ok=True)
-                    os.makedirs(os.path.join(slant_path, subject_folder, 'out', 'pre'), exist_ok=True)
-                    os.makedirs(os.path.join(slant_path, subject_folder, 'out', 'post'), exist_ok=True)
-                    os.makedirs(os.path.join(slant_path, subject_folder, 'out', 'dl'), exist_ok=True)
-
-                    # Copy the file to the new directory
-                    os.system(f'cp {file_path} {os.path.join(slant_path, subject_folder, "in", "t1w.nii.gz")}')
-
-root_directory = '/nfs/masi/saundam1/datasets/MP2RAGE'
-csv_file_path = '/nfs/masi/saundam1/datasets/MP2RAGE_SIR_qMT/ground_truth_subjects.csv'
-column_with_subject_ids = 'Subject'
-
-mp2rage_files_list = find_mp2rage_files(root_directory, csv_file_path, column_with_subject_ids)
+    # Copy the file to the new directory
+    os.system(f'cp {file_path} {os.path.join(slant_path, str(subj_id), "in", "t1w.nii.gz")}')
